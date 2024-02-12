384ea7c5ca41652edba47e7de3bb8fcdd74e741b
Crowdbotics <team@crowdbotics.com>
2024-02-12 13:44:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tsunami-46255",
+  url: "https://tsunami-46255.botics.co",
+  api: "https://tsunami-46255.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
